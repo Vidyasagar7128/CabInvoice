5a97801fe8cfda7fdbb051d91b99fe3bd8211b25
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-10-12 23:43:22 +0530

    Added: Total fare of journey

---
+++ CabInvoice/CabInvoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoice
+{
+    class Cab
+    {
+        public double cost { get; set; }
+        public double kilometer { get; set; }
+        public double minute { get; set; }
+    }
+}
